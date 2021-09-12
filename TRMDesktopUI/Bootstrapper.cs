c8ff28191fb8299f7272f886ec9e64cc1f8a78bb
MetaAttack <49219375+mohamedsakeel@users.noreply.github.com>
2021-09-12 11:54:11 +0530

    Add WPF Project with MVVM
    Adds our WPF user interface and cofigures wih caliburn micro.
    also include simple container system

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -11,14 +11,49 @@ namespace TRMDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
         }
 
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            //reflections
+
+            GetType().Assembly.GetTypes()                           //gettype from the application
+                .Where(type => type.IsClass)                        //limitting to class types
+                .Where(type => type.Name.EndsWith("ViewModel"))     //getting all the classes that end in view model
+                .ToList() 
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType)); 
+        }
+
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);  
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
