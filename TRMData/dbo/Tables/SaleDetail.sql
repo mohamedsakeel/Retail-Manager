9dad860e076be073ab9c5a2f1642132373afea96
MetaAttack <49219375+mohamedsakeel@users.noreply.github.com>
2021-09-12 21:29:37 +0530

    Register DB tables
    The tables for the basic regster system

---
+++ TRMData/dbo/Tables/SaleDetail.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [SaleID] INT NOT NULL, 
+    [ProductID] INT NOT NULL,
+    [Quantity] INT NOT NULL DEFAULT 1,
+    [PurchasePrice] MONEY NOT NULL, 
+    [Tax] MONEY NOT NULL DEFAULT 0
+    
+)
